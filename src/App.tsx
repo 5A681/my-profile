7f50320d1c6d987c186746daf467731969c1551a
5A681 <heartboxengineer@gmail.com>
2025-03-24 22:42:13 +0700

    update footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Content from "./components/Content/Content";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
+import "boxicons/css/boxicons.min.css";
 
 function App() {
   return (
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,31 +1,36 @@
 import "./Footer.css";
+import "boxicons";
 export default function Footer() {
   return (
-    <div className="footer">
-      <li>
-        <div>
-          <h1>aa</h1>
-          <h3>aa</h3>
-        </div>
-      </li>
-      <li>
-        <div>
-          <h1>aa</h1>
-          <h3>aa</h3>
-        </div>
-      </li>
-      <li>
-        <div>
-          <h1>aa</h1>
-          <h3>aa</h3>
-        </div>
-      </li>
-      <li>
-        <div>
-          <h1>aa</h1>
-          <h3>aa</h3>
-        </div>
-      </li>
+    <div className="footer-container">
+      <div className="footer">
+        <li>
+          <div>
+            <h1>Phone</h1>
+            <h3>123-456-7890</h3>
+          </div>
+        </li>
+        <li>
+          <div>
+            <h1>Email</h1>
+            <h3>heartboxengineer@gmail.com</h3>
+          </div>
+        </li>
+        <li>
+          <div>
+            <h1>Follow Me</h1>
+            <h1>
+              <i className="bx bxl-linkedin-square"></i>
+            </h1>
+          </div>
+        </li>
+        <li>
+          <div>
+            <h1>aa</h1>
+            <h3>aa</h3>
+          </div>
+        </li>
+      </div>
     </div>
   );
 }
